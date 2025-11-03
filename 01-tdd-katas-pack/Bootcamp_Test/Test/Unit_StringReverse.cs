7d2c039ea6e242c22f3523783c15ff0562104a19
abdessamadjadid <abdessamadjadid@gmail.com>
2025-11-02 20:53:21 -0600

    Add .gitignore and clean VS cache files

---
+++ 01-tdd-katas-pack/Bootcamp_Test/Test/Unit_StringReverse.cs
@@ -0,0 +1,25 @@
+﻿namespace Bootcamp_Test.Test
+{
+    public class Unit_StringReverse
+    {
+        [Theory]
+        [InlineData("ronaldo","odlanor")]
+        public void StringReverse_Test(string input, string expected)
+        {
+
+            // Arrange
+            var sut = new StringReverses();
+            var chars = input.ToCharArray();
+
+            // Act
+            sut.StringReverse(chars);
+            var result = new string(chars);
+
+            // Assert
+            Assert.Equal(expected, result);
+
+
+
+        }
+    }
+}
---
+++ 01-tdd-katas-pack/Bootcamp_Test/src/BowlingScore_Skeleton.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class BowlingScore_Skeleton
+{
+	public BowlingScore_Skeleton()
+	{
+	}
+}
